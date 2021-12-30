a6be8e02773ce63b5d3a40680eb935882c676b0e
tjdwlslrj <tjdwlslrj@naver.com>
2021-12-30 20:39:19 +0900

    Using Selenium / Login Baekjoon

---
+++ Login.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import time
+
+def login(driver):
+    action = ActionChains(driver)
+    action.send_keys('tjdwlslrj').perform()
+    time.sleep(1)
+    driver.find_elements_by_css_selector('.form-control')[1].send_keys('1903qo')
+    driver.find_element_by_css_selector('.btn-u.pull-right').click()
