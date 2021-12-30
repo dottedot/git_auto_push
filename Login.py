82e8227de7061c00aae0f6faf6194524ff2a165c
tjdwlslrj <tjdwlslrj@naver.com>
2021-12-30 21:40:39 +0900

    Using Selenium / Login Baekjoon

--- Login.py
+++ Login.py
@@ -4,7 +4,7 @@ import time
 
 def login(driver):
     action = ActionChains(driver)
-    action.send_keys('tjdwlslrj').perform()
+    action.send_keys('[BAEKJOON_ID]').perform()
     time.sleep(1)
-    driver.find_elements_by_css_selector('.form-control')[1].send_keys('1903qo')
+    driver.find_elements_by_css_selector('.form-control')[1].send_keys('[BAEKJOON_PASSWD]')
     driver.find_element_by_css_selector('.btn-u.pull-right').click()
