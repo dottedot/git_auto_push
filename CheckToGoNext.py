722a3cc7af57163ecabef91c931772d2b8d07ad3
tjdwlslrj <tjdwlslrj@naver.com>
2022-01-14 00:40:27 +0900

    Solve CAPTCHA:(1.01)

--- CheckToGoNext.py
+++ CheckToGoNext.py
@@ -1,9 +1,16 @@
-import time
-def Check(driver):
+#-*- coding: utf-8 -*-
+
+import time, priv_user
+
+def check(driver):
     flag = 1
+
     while flag:
         time.sleep(1)
-        if driver.current_url == 'https://www.acmicpc.net/status?group_id=13131':
+
+        if driver.current_url == priv_user.BOJ_CAPTCHA:
             flag = 0
         else:
-            print('Getchar 푸는중...')
+            print('CAPTCHA 푸는중...')
+    
+    print('\n######### START #########\n')
