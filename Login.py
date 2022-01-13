4e134a8bcdfed7aa2bf076b10146e133f933528e
tjdwlslrj <tjdwlslrj@naver.com>
2022-01-14 00:42:23 +0900

    Login BOJ:(1.01)

--- Login.py
+++ Login.py
@@ -1,10 +1,13 @@
+#-*- coding: utf-8 -*-
+
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
-import time
+import time, priv_user
 
+## Login BOJ
 def login(driver):
     action = ActionChains(driver)
-    action.send_keys('[BAEKJOON_ID]').perform()
+    action.send_keys(priv_user.BOJ_ID).perform()
     time.sleep(1)
-    driver.find_elements_by_css_selector('.form-control')[1].send_keys('[BAEKJOON_PASSWD]')
+    driver.find_elements_by_css_selector('.form-control')[1].send_keys(priv_user.BOJ_PWD)
     driver.find_element_by_css_selector('.btn-u.pull-right').click()
